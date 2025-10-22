6f4a9908bf4589649e0e3806571a80f9275c7adc
Eduardo Teixeira <eteixeira566@gmail.com>
2025-10-21 23:35:46 -0300

    Commit

--- ClinicaApp/Services/MedicoService.cs
+++ ClinicaApp/Services/MedicoService.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ClinicaApp.Models;
 
-namespace ClinicaApp.Services
+namespace ClinicaApp.Services;
+
+public class MedicoService
 {
-    internal class MedicoService
+    private readonly string FilePath;
+    private List<Medico> medicos;
+
+    public MedicoService()
     {
+        // Local do assembly (DLL/ Exe) que está sendo executado
+        var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        
+        // Diretório do assembly
+        var assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? "";
+
+        // sobe 3 niveis a partir do bin/Debug/net8.0 para chegar na raiz do projeto
+
     }
 }
